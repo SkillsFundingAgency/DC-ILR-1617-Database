f9d6c07e57010bf288a9a0ca33c91da247f14cd4
Peter Chamley <13586812+PJChamley@users.noreply.github.com>
2018-11-21 09:19:52 +0000

    Fix User Account name.

--- src/ESFA.DC.ILR1617.DataStore.Database/Security/RoleMemberships.sql
+++ src/ESFA.DC.ILR1617.DataStore.Database/Security/RoleMemberships.sql
@@ -1,8 +1,8 @@
 ﻿
 GO
-ALTER ROLE [DataProcessing] ADD MEMBER [ILR1819DataStore_RW_User];
+ALTER ROLE [DataProcessing] ADD MEMBER [ILR1617DataStore_RW_User];
 GO
-ALTER ROLE [DataViewing] ADD MEMBER [ILR1819DataStore_RO_User];
+ALTER ROLE [DataViewing] ADD MEMBER [ILR1617DataStore_RO_User];
 GO
 ALTER ROLE [db_datareader] ADD MEMBER [User_DSCI];
 GO
